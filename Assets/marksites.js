add81af996e995869a613be119ee9da8a4cd55d5
Chris Hughes <chughes@teleport.com>
2011-05-22 19:44:54 -0700

    Commit after changes at end of TY2009 season

--- Assets/marksites.js
+++ Assets/marksites.js
@@ -1,20 +1,20 @@
 
 	// Global variables
 	var data = [
-	{ name: "Vancouver Main Library", open: "Opens Feb 3rd 2010, Wed 4-8pm<br />Fri 10am-2pm, Sat 10am-4pm", lat:"45.629975", lng:"-122.661538", url: "#MainLib" },
-	{ name: "Marshall Community Center", open: "Opens Feb 2cnd 2010, <br />Tues, Thurs 12-4pm", lat:"45.633516", lng:"-122.661581", url: "#Marshall" },
-	{ name: "Cascade Park Community Library", open: "Opens Feb 2cnd 2010, <br />Tues, Wed 12pm-4pm", lat:"45.626119", lng:"-122.534895", url: "#Cascade" },
-	{ name: "Camas Public Library", open: "Opens Feb 2cnd 2010, <br />Tues 3-6pm, Thur 2-5pm", lat:"45.586999", lng:"-122.402158", url: "#Camas" },
-	{ name: "First Baptist Church", open: "Opens Feb 3rd 2010, <br />Wed-Fri 11am-3pm", lat:"45.642180", lng:"-122.669134", url: "#FBC" },
-	{ name: "Glenwood Place Senior Living", open: "Opens Feb 1st 2010, <br />Mon, Tues 10am-6pm", lat:"45.663202", lng:"-122.589526", url: "#Glenwood" },
-	{ name: "Battle Ground Community Library", open: "Opens Feb 3rd 2010, <br />Wed 3-7pm, Fri 1-5pm", lat:"45.786495", lng:"-122.550173", url: "#BattleGround" },
-	{ name: "Three Creeks Community Library", open: "Opens Feb 2cnd 2010, <br />Tues-Wed 1pm-5pm, Thur 3-7pm", lat:"45.721282", lng:"-122.665615", url: "#ThreeCreeks" },
-	{ name: "McClelland Center", open: "Opens Feb 2cnd 2010, <br />Tues, Thurs 9am-3pm", lat:"46.129255", lng:"-122.932463", url: "#McCell" },
-	{ name: "Longview Public Library", open: "Opens Feb 5th 2010, <br />Fri-Sat 12:30-4:30pm", lat:"46.139820", lng:"-122.937655", url: "#Longlib" },
-	{ name: "Fibre Credit Union", open: "Opens Feb 2cnd 2010, <br />Tues 3:30-6:30pm", lat:"46.128333", lng:"-122.937613", url: "#Fibre" },
-	{ name: "Monticello Middle School", open: "Opens Feb 4th 2010, <br />Thurs 3:30-7:00pm", lat:"46.138214", lng:"-122.957654", url: "#Monticello" },	
-	{ name: "Community Housing Resource Center", open: "Opens Feb 2cnd 2010, <br />Tue-Thur 10-2pm By Appt Only", lat:"45.642030", lng:"-122.601242", url: "#CHRC" },
-	{ name: "Cathlamet River Room", open: "Opens Wed Feb 10,17, Mar 10,17, Apr 7,14 1-3:30pm<br />Sat Feb 20, Mar 20 11:30am-3:30pm", lat:"46.186932", lng:"-123.355179", url: "#Cathlamet" }
+	{ name: "Vancouver Main Library", open: "Closed until Feb 2011", lat:"45.629975", lng:"-122.661538", url: "#MainLib" },
+	{ name: "Marshall Community Center", open: "Closed until Feb 2011", lat:"45.633516", lng:"-122.661581", url: "#Marshall" },
+	{ name: "Cascade Park Community Library", open: "Closed until Feb 2011", lat:"45.626119", lng:"-122.534895", url: "#Cascade" },
+	{ name: "Camas Public Library", open: "Closed until Feb 2011", lat:"45.586999", lng:"-122.402158", url: "#Camas" },
+	{ name: "First Baptist Church", open: "Closed until Feb 2011", lat:"45.642180", lng:"-122.669134", url: "#FBC" },
+	{ name: "Glenwood Place Senior Living", open: "Closed until Feb 2011", lat:"45.663202", lng:"-122.589526", url: "#Glenwood" },
+	{ name: "Battle Ground Community Library", open: "Closed until Feb 2011", lat:"45.786495", lng:"-122.550173", url: "#BattleGround" },
+	{ name: "Three Creeks Community Library", open: "Closed until Feb 2011", lat:"45.721282", lng:"-122.665615", url: "#ThreeCreeks" },
+	{ name: "McClelland Center", open: "Closed until Feb 2011", lat:"46.129255", lng:"-122.932463", url: "#McCell" },
+	{ name: "Longview Public Library", open: "Closed until Feb 2011", lat:"46.139820", lng:"-122.937655", url: "#Longlib" },
+	{ name: "Fibre Credit Union", open: "Closed until Feb 2011", lat:"46.128333", lng:"-122.937613", url: "#Fibre" },
+	{ name: "Monticello Middle School", open: "Closed until Feb 2011", lat:"46.138214", lng:"-122.957654", url: "#Monticello" },	
+	{ name: "Community Housing Resource Center", open: "Closed until Feb 2011", lat:"45.642030", lng:"-122.601242", url: "#CHRC" },
+	{ name: "Cathlamet River Room", open: "Closed until Feb 2011", lat:"46.186932", lng:"-123.355179", url: "#Cathlamet" }
 	];
 
 	var map;
